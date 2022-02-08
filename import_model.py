bef063a4edf03358b37d7b01cb3267100f7b621d
Luis Vizcaya <luis.vizcaya@woodmac.com>
2022-02-08 13:44:54 -0600

    "R2 = 0.75 with linear regression"

--- import_model.py
+++ import_model.py
@@ -11,50 +11,33 @@ import matplotlib.pyplot as plt
 
 DATA_RAW = pd.read_csv('Import_Data_v4.csv')
 DATA_RAW.dropna(inplace=True)
-DATA_RAW = DATA_RAW.iloc[len(DATA_RAW)-1000:-1,:]
-
-loads = DATA_RAW[['DEMAND-MISO','DEMAND-PJM','WIND-MISO','WIND-PJM','IMPORT-PJM']]
+#print(DATA_RAW.columns[1])
+AVG2 = pd.DataFrame()
+STD2 = pd.DataFrame()
+#DATA_RAW = DATA_RAW.iloc[len(DATA_RAW)-1000:-1,:]
+#print(DATA_RAW.index)
+#A = DATA_RAW.apply(lambda x: AVGS[(AVGS.DayOfWeek==x.DayOfWeek) & (AVGS.Hour==1)]['DEMAND-MISO'],axis = 1)
+#print(AVGS.loc[(AVGS.DayOfWeek=='Monday') & (AVGS.Hour==0)])
+#DATA_RAW['PRUEBA'] = DATA_RAW['DayOfWeek', 'Hour'].map(AVGS.set_index(['DayOfWeek', 'Hour'])['DEMAND-MISO'])
+#DATA_RAW.set_index(['DayOfWeek', 'Hour'])
 
 AVGS = DATA_RAW.groupby(['DayOfWeek', 'Hour'], as_index = False).mean()
 AVGS.reset_index() #reset the index
-#print(AVGS.loc[ (AVGS.DayOfWeek=='Monday') & (AVGS.Hour ==0)])
-
-
-AVGS1 = DATA_RAW.index.map(lambda d: AVGS.loc[ (AVGS.DayOfWeek==d.DayOfWeek) & (AVGS.Hour==d.Hour)])
-
-print( AVGS1)
-
-
-#AVGS.reset_index(inplace=True)
-
 STDVS = DATA_RAW.groupby(['DayOfWeek', 'Hour']).std()
 STDVS.reset_index(inplace=True)
 
-DEVS = pd.DataFrame()
-#print(AVGS)
+for c in DATA_RAW.columns[3:]:
+    AVG2[c] = [ float(AVGS[(AVGS.DayOfWeek==x) & (AVGS.Hour==y)][c]) for x,y in zip(DATA_RAW['DayOfWeek'],DATA_RAW['Hour'])]
+    STD2[c] = [ float(STDVS[(STDVS.DayOfWeek==x) & (STDVS.Hour==y)][c]) for x,y in zip(DATA_RAW['DayOfWeek'],DATA_RAW['Hour'])]
+    DATA_RAW[c] = (DATA_RAW[c]-AVG2[c])/STD2[c]
 
+DATA_RAW = DATA_RAW[:-1]
 
-DATA = pd.DataFrame()
-for r in loads.index:
-    for c in loads.columns:
-
-        DIA = DATA_RAW['DayOfWeek'][r]
-        HORA = DATA_RAW['Hour'][r]
-        a = (AVGS.loc[:,'DayOfWeek']==DIA) & (AVGS.loc[:,'Hour']==HORA)
-        PROM = AVGS.loc[a]
-        a = (STDVS.loc[:,'DayOfWeek']==DIA) & (STDVS.loc[:,'Hour']==HORA)
-        SD = STDVS.loc[a]
-        
-        PROM = float(PROM[c])
-        SD = float(SD[c])
-        
-        
-        #loads.loc[r,c] = (loads.loc[r,c]-PROM)/SD
-
+loads = DATA_RAW.iloc[:,3:]
+print(loads)
 imp = loads[['IMPORT-PJM']]
 loads.drop('IMPORT-PJM', axis=1, inplace=True)
-
-    
+  
 regr = linear_model.LinearRegression()
 loads_train, loads_test, imp_train, imp_test = train_test_split(loads, imp, test_size=0.2, random_state=0)
 
@@ -65,5 +48,4 @@ imp_pred = regr.predict(loads_test)
 
 
 print('R2: ',r2_score(imp_test,imp_pred))
-
 
