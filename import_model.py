9c4f60de5a76d34183ccdaece37700460f1dbbac
Luis Vizcaya <luis.vizcaya@woodmac.com>
2022-02-09 16:11:16 -0600

    "feature selection semi automated"

--- import_model.py
+++ import_model.py
@@ -15,7 +15,6 @@ from scipy import stats
 
 DATA_RAW = pd.read_csv('Import_Data_v5.csv')
 DATA_RAW.dropna(inplace=True)
-#print(DATA_RAW.columns[1])
 AVG2 = pd.DataFrame()
 STD2 = pd.DataFrame()
 #DATA_RAW = DATA_RAW.iloc[len(DATA_RAW)-1000:-1,:]
@@ -24,6 +23,11 @@ STD2 = pd.DataFrame()
 #print(AVGS.loc[(AVGS.DayOfWeek=='Monday') & (AVGS.Hour==0)])
 #DATA_RAW['PRUEBA'] = DATA_RAW['DayOfWeek', 'Hour'].map(AVGS.set_index(['DayOfWeek', 'Hour'])['DEMAND-MISO'])
 #DATA_RAW.set_index(['DayOfWeek', 'Hour'])
+#loads_train, loads_test, imp_train, imp_test = train_test_split(loads, imp, test_size=0.2, random_state=0)
+#print('load_train: ',loads_test)
+#print('imp_train:',imp_test)
+#print(DATA_RAW.columns[1])
+
 
 AVGS = DATA_RAW.groupby(['DayOfWeek', 'Hour'], as_index = False).mean()
 AVGS.reset_index() #reset the index
@@ -39,48 +43,61 @@ DATA_RAW = DATA_RAW[:-1]
 
 
 loads = DATA_RAW.iloc[:,3:]
+loads.drop(['LMP-WEST-PJM','LMP-WESTERN-PJM','LMP-MINNESOTA-MISO','LMP-ILLINOIS-PJM','LMP-MICHIGAN-PJM','LMP-COMED-PJM','LMP-AEP-PJM','NATGAS-TRANSCO-Z6','NATGAS-TETCO','NATGAS-CHICAGO','TEMPERATURE-WESTERN-PJM'], axis=1, inplace=True)
 
-loads = loads[(np.abs(stats.zscore(loads)) < 1.5).all(axis=1)]
-
+loads = loads[(np.abs(stats.zscore(loads)) < 2).all(axis=1)]
 loads.dropna(inplace = True)
+loads.reset_index(inplace = True)
+elim = [4]
+loads.drop(loads.columns[elim],axis = 1, inplace = True)
+
 loads = loads.diff()[1:]
-imp = loads[['IMPORT-PJM']]
+aux_loads = loads
 
-#elim = [4,5,9,11]
-#loads.drop(loads.columns[elim],axis = 1, inplace = True)
-#loads.drop(['IMPORT-PJM','LMP-WEST-PJM','LMP-WESTERN-PJM','LMP-MINNESOTA-MISO','LMP-ILLINOIS-PJM','LMP-MICHIGAN-PJM','LMP-COMED-PJM','LMP-AEP-PJM'], axis=1, inplace=True)
-loads.drop(['IMPORT-PJM'], axis=1, inplace=True)
+MAX_R2 =  0
 
-regr = linear_model.LinearRegression()
-train_len = int(len(loads)*0.8)
-print('train_len:', train_len)
-loads_train = loads[:train_len]
-print('len(load_train): ', len(loads_train))
-loads_test = loads[train_len:]
-imp_train = imp[:train_len]
-imp_test = imp[train_len:]
+best_col =-1
 
-#loads_train, loads_test, imp_train, imp_test = train_test_split(loads, imp, test_size=0.2, random_state=0)
-#print('load_train: ',loads_test)
-#print('imp_train:',imp_test)
-
-
-
-regr.fit(loads_train, imp_train)
-imp_pred = regr.predict(loads_test)
-print('R2: before SARIMA',r2_score(imp_test,imp_pred))
+for col in range(4,len(loads.columns)-2):
+    print('running col:', col)
+    loads = aux_loads.drop(aux_loads.columns[col],axis = 1)
+    best_cut = 0
+    cut = 0
+    loads = aux_loads[cut:len(aux_loads)]
+    while (cut <1000):
+        loads = aux_loads[cut:len(aux_loads)]
+        train_len = int(len(loads))
+        print('train_len : ', train_len)
+        imp = loads[['IMPORT-PJM']]
+        loads.drop(['IMPORT-PJM'], axis=1, inplace=True)
+        regr = linear_model.LinearRegression()
+        loads_train = loads[:train_len-168]
+        print('len(load_train): ', len(loads_train))
+        loads_test = loads[train_len-168:]
+        imp_train = imp[:train_len-168]
+        imp_test = imp[train_len-168:]
+        regr.fit(loads_train, imp_train)
+        imp_pred = regr.predict(loads_test)
+        R2 = r2_score(imp_test,imp_pred)
+        if R2 > MAX_R2:
+            MAX_R2 = R2
+            best_cut = cut
+            best_col = col
+        cut += 168
+print('Max R2:',MAX_R2)
+print('best cut:',best_cut)
+print('best col to drop: ', loads.columns[best_col])
 
 pred_train= regr.predict(loads_train)   # predicted values for training data
-
 residual = imp_train - pred_train       # residual in training data
 
 
-plt.plot(imp)
+#plt.plot(imp)
 #plt.legend()
 
-acf_vals = acf(residual)
-num_lags = 20
+#acf_vals = acf(residual)
+#num_lags = 20
 #plt.bar(range(num_lags), acf_vals[:num_lags])
-plt.show()
+#plt.show()
 
 
