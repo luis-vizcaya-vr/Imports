fd20891a1a62f46bf407d16b147b810c79a9fb72
Luis Vizcaya <luis.vizcaya@woodmac.com>
2022-03-10 15:13:05 -0600

    applying different best method to the dataset

--- Gas_Forecast.py
+++ Gas_Forecast.py
@@ -14,79 +14,50 @@ from darts.dataprocessing.transformers import (Scaler, MissingValuesFiller, Mapp
 import winsound
 import seaborn as sn
 
-
-
-
-
+START_POINT = 0.90
 FORECAST_PERIOD = 14
+FUELS_TO_FORECAST = list(pd.read_csv('FUEL_SIDS.csv')['FUEL'])
+#print('FUELS TO FORECAST: ', FUELS_TO_FORECAST)
 temp = pd.read_csv('NATGAS-DATA_V3.csv', usecols= [0,11])
-
 #corrMatrix = data.corr()
 #sn.heatmap(corrMatrix, annot=True)
 #plt.show()
-df = pd.read_csv('NATGAS-DATA_V3.csv', usecols= [0,2])
+
+fuel_mapping = pd.read_csv('fuel_mapping.csv')
+fuel_mapping.set_index('in_stack', drop = True, inplace=True)
+fuel_dict = fuel_mapping.to_dict()
+
+print('fuel_dict: ', fuel_dict['in_fuel_price'])
+
+Fuel = fuel_dict['in_fuel_price'][FUELS_TO_FORECAST[3]]
+df = pd.read_csv('FUEL_PRICES_DATA.csv', usecols= ['Date',Fuel])
+    
 
 End_Date = pd.datetime(2021,12,31)
 Start_Date = pd.datetime(2021,5,1)
 df['Date'] = pd.DatetimeIndex(df['Date'])
 df = df.loc[(df['Date'] <= End_Date) & (df['Date'] >= Start_Date)]
 temp['Date'] = pd.DatetimeIndex(temp['Date'])
-temp = temp.loc[(temp['Date'] <= End_Date) & (temp['Date'] >= Start_Date)]
-
 
 series = TimeSeries.from_dataframe(df, time_col = 'Date', freq = 'd', fill_missing_dates = True)
-#scaler = Scaler()
-#series = scaler.fit_transform(series)
-
-series.plot(label = 'real price')
-#plt.show()
-
-
+series.plot(label = Fuel)
 temp_serie = TimeSeries.from_dataframe(temp, time_col = 'Date', freq = 'd', fill_missing_dates = True)
-#series.plot(label = 'temperature')
-
-train, val = series[:-FORECAST_PERIOD], series[-FORECAST_PERIOD:]
-
-def eval_model(model,train):
-  res = {}
-  print('Running: ', model)
-  model.fit(train)
-  forecast = model.predict(len(val))
-  res['MAPE'] = drt.metrics.metrics.mape(val, forecast)
-  res['MASE'] = drt.metrics.metrics.mase(val, forecast,train)
-  res['R2'] = drt.metrics.metrics.r2_score(val, forecast)
-  res['Forecast'] = forecast  
-  return res
-
-def Best_model(model_list, train, metric = 'MAPE'):
-  Best = ''
-  Plot_Id = 100
-  results = {}
-  Max_Dev = float("Inf")
-  for m in model_list:
-    r = eval_model(m,train)
-    Plot_Id += 1
-    print(metric,' :', r[metric])
-    if r[metric] < Max_Dev:
-      Max_Dev = r[metric]
-      results = r
-      results['Model'] = str(m)
-  return results
-
 
 parameters = {
     "ARIMA":
-        {"p": [1],
-        "q": [1],
+        {"p": [0,1,2,3],
+        "q": [4,5,6,7],
         "d": [0]
        },
 
     "ExponentialSmoothing":
-        {"trend": [ModelMode.NONE,ModelMode.MULTIPLICATIVE]},
+        {"trend": [ModelMode.NONE, ModelMode.MULTIPLICATIVE, ModelMode.ADDITIVE]},
 
     "T":
         {
-          "theta":[0.5, 1],
+          "theta":[ 0.4, 0.5, 0.6],
+          "model_mode": [ModelMode.MULTIPLICATIVE, ModelMode.ADDITIVE],
+          "trend_mode": [TrendMode.LINEAR, TrendMode.EXPONENTIAL]
         },
         
     "VARIMA":
@@ -100,19 +71,37 @@ parameters = {
         },
     } 
 
-#print("Gridsearch Exponential..")
+def Best_model_V2(model_list, train, metric = 'MAPE', exog = None):
+  results = {}
+  Max_Dev = float("Inf")
+  for m in model_list:
+    r = m.historical_forecasts(train, start = START_POINT, forecast_horizon = 14, verbose = False,  future_covariates = exog)
+    met =  metrics.metrics.mape(r, series)
+    print(metric,' :', met)
+    if met < Max_Dev:
+      Max_Dev = met
+      results['Output'] = r
+      results['Model'] = m
+  print('Best Model: ',results['Model'])
+  return results
 
+models_list = []
 Tet = FourTheta.gridsearch(parameters = parameters["T"], series = series, forecast_horizon = FORECAST_PERIOD, metric = metrics.metrics.mape, n_jobs = -1 ,  future_covariates=temp_serie)
+models_list.append(Tet[0])
 Arima_model = ARIMA.gridsearch(parameters = parameters["ARIMA"], forecast_horizon = FORECAST_PERIOD, series = series,  verbose = False, n_jobs = -1,  future_covariates=temp_serie)
+models_list.append(Arima_model[0])
 Expon = ExponentialSmoothing.gridsearch(parameters = parameters["ExponentialSmoothing"], forecast_horizon= FORECAST_PERIOD, series = series,  verbose = False, n_jobs = -1,  future_covariates=temp_serie)
+models_list.append(Expon[0])
+
 #Auto_Arima = AutoARIMA()
 #Auto_Arima.fit(series = series,  future_covariates = temp_serie)
-
 #Varima = VARIMA.gridsearch(parameters = parameters["VARIMA"], forecast_horizon = FORECAST_PERIOD, series = series)
 #Kalman = KalmanForecaster.gridsearch(series = series,parameters = parameters["KALMAN"],  forecast_horizon = FORECAST_PERIOD,  future_covariates=temp_serie)
-#a = Best_model(model_list, train, metric = 'MAPE')
-#print('Historical Forecast..')
+a = Best_model_V2(models_list, series, metric = 'MAPE', exog = temp_serie)
+a['Output'].plot(label = str(a["Model"]))
 
+
+'''
 #h = Kalman[0].historical_forecasts(series = series, start = 0.8, forecast_horizon= FORECAST_PERIOD, verbose = False,  future_covariates=temp_serie)
 #mape = metrics.metrics.mape(h, series)
 #h.plot(label = 'Arima: '+ str(mape))
@@ -133,23 +122,12 @@ h = Arima_model[0].historical_forecasts(series, start = 0.8, forecast_horizon= F
 mape = metrics.metrics.mape(h, series)
 h.plot(label = 'Arima: '+ str(mape))
 
-
-'''
 h = Varima[0].historical_forecasts(series, start = 0.8, forecast_horizon= FORECAST_PERIOD, verbose = False,  future_covariates=temp_serie)
 mape = metrics.metrics.mape(h, series)
 h.plot(label = 'Varima: '+ str(mape))
 
-#series.to_csv("serie.csv")
-#h.to_csv("gas_price.csv")
 '''
 winsound.Beep(440, 100)
 plt.ylim(0,7)
 plt.show()
 plt.savefig('model.jpg')
-
-
-
-
-#h = Kalman[0].historical_forecasts(series, start = 0.4, forecast_horizon= FORECAST_PERIOD)
-#h = Fourier[0].historical_forecasts(series, start = 0.4, forecast_horizon= FORECAST_PERIOD)
-#print(metrics.metrics.mape(h, series))'''
