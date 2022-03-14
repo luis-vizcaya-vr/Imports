1b0951d9b940f9d75293df80829bc2b8b262e1d5
Luis Vizcaya <luis.vizcaya@woodmac.com>
2022-03-14 17:02:46 -0500

    output file updated

--- Gas_Forecast.py
+++ Gas_Forecast.py
@@ -16,24 +16,10 @@ from darts.dataprocessing.transformers import (Scaler, MissingValuesFiller, Mapp
 import winsound
 import seaborn as sn
 
-START_POINT = 0.90
-FORECAST_PERIOD = 14
-Today = datetime.date.today()
-Yesterday = Today - timedelta(days = 1)
-Forecast_Date = Today + timedelta(days = FORECAST_PERIOD)
-Fuel_list = list(pd.read_csv('FUEL_SIDS.csv')['FUEL'])
-Fuel_list = Fuel_list[0:2]
-
-temp = pd.read_csv('NATGAS-DATA_V3.csv', usecols= [0,11])
-fuel_mapping = pd.read_csv('fuel_mapping.csv')
-fuel_mapping.set_index('in_stack', drop = True, inplace=True)
-fuel_dict = fuel_mapping.to_dict()
-Fuel_Data = pd.read_csv('FUEL_PRICES_DATA.csv')
-
 parameters = {
     "ARIMA":
-        {"p": [1,2,3],
-        "q": [4,5,6],
+        {"p": [1],
+        "q": [5],
         "d": [0]
        },
 
@@ -42,7 +28,7 @@ parameters = {
 
     "T":
         {
-          "theta":[ 0.4, 0.5, 0.6],
+          "theta":[0.3, 0.4, 0.5, 0.6, 1],
           "model_mode": [ModelMode.MULTIPLICATIVE, ModelMode.ADDITIVE],
           "trend_mode": [TrendMode.LINEAR, TrendMode.EXPONENTIAL]
         },
@@ -58,59 +44,153 @@ parameters = {
         },
     } 
 
+START_POINT = 0.92
+FORECAST_PERIOD = 14
+Today = datetime.date.today()
+Forecast_Date = Today + timedelta(days = FORECAST_PERIOD)
+Fuel_list = list(pd.read_csv('FUEL_SIDS.csv')['FUEL'])
+print('Fuel list:',Fuel_list)
+Fuel_list = Fuel_list[0:1]
+temp = pd.read_csv('NATGAS-DATA_V3.csv', usecols= [0,11])
+fuel_mapping = pd.read_csv('fuel_mapping.csv')
+fuel_mapping.set_index('in_stack', drop = True, inplace=True)
+fuel_dict = fuel_mapping.to_dict()
+Fuel_Data = pd.read_csv('FUEL_PRICES_DATA.csv')
+Fuel_Data['Date'] = pd.DatetimeIndex(Fuel_Data['Date'])
+      
+
 def Best_model_V2(model_list, train, metric = 'MAPE', exog = None):
   results = {}
   Max_Dev = float("Inf")
+  #Num_models =  len(model_list)
+  #fig, axs = plt.subplots(Num_models)
+  #fig.suptitle('Vertically stacked subplot')
+  i = 0
   for m in model_list:
-    r = m.historical_forecasts(train, start = START_POINT, forecast_horizon = 14, verbose = False,  future_covariates = exog)
-    met =  metrics.metrics.mape(r, train)
+    r = m.historical_forecasts(train, start = START_POINT, forecast_horizon = 14, verbose = False, past_covariates = exog, future_covariates = exog)
+    #axs[i].plot(r)
+    #met =  metrics.metrics.mape(r, train)
+    #met =  metrics.metrics.ope(r, train)
+
+    met =  metrics.metrics.mse(r, train)
+    results['MAPE'] = metrics.metrics.mape(r, train)
+
     print(metric,' :', met)
+    
     if met < Max_Dev:
       Max_Dev = met
       results['Output'] = r
       results['Model'] = m
+      results['metric'] = met
+    i+=1
+  #plt.show()
   print('Best Model: ',results['Model'])
   return results
 
-def To_Date_Data(Data, var_list):
+def To_Date_Data(Data, var_list, period =14):
   Today = datetime.date.today() 
-  Forecast_Date = Today + timedelta(days = 13)
-  End_Date =  pd.to_datetime(Data['Date'].iloc[-1])
-  print('Today: ', Today)
-  print('Forecast Date: ', Forecast_Date)
-  print('End Date: ', End_Date)
-
+  Forecast_Date = Today + timedelta(days = period)
+  Start_Date = pd.datetime(2021,5,1)
   Forecast_Date =  pd.to_datetime(Forecast_Date)
-  FORECAST_PERIOD = max((Forecast_Date - End_Date).days,0)
+  test = Data['Date'].dropna()
+  Last_Index = test.index[-1]
+  Last_Date = Data.loc[Last_Index,'Date']
+  d_range = pd.date_range(start = Last_Date, end = Forecast_Date)
 
-  for var in var_list:
-    Fuel = fuel_dict['in_fuel_price'][var]
-    df = Fuel_Data[['Date', Fuel]]
-    df['Date'] = pd.DatetimeIndex(df['Date'])
-    Start_Date = pd.datetime(2021,5,1)
-    df = df.loc[(df['Date'] <= End_Date) & (df['Date'] >= Start_Date)]
-    temp['Date'] = pd.DatetimeIndex(temp['Date'])    
-    series = TimeSeries.from_dataframe(df, time_col = 'Date', freq = 'd', fill_missing_dates = True)
-    series.plot(label = Fuel)
-    temp_serie = TimeSeries.from_dataframe(temp, time_col = 'Date', freq = 'd', fill_missing_dates = True)
-    models_list = []
-    Tet = FourTheta.gridsearch(parameters = parameters["T"], series = series, forecast_horizon = FORECAST_PERIOD, metric = metrics.metrics.mape, n_jobs = -1 ,  future_covariates=temp_serie)
-    models_list.append(Tet[0])
-    Arima_model = ARIMA.gridsearch(parameters = parameters["ARIMA"], forecast_horizon = FORECAST_PERIOD, series = series,  verbose = False, n_jobs = -1,  future_covariates=temp_serie)
-    models_list.append(Arima_model[0])
-    Expon = ExponentialSmoothing.gridsearch(parameters = parameters["ExponentialSmoothing"], forecast_horizon= FORECAST_PERIOD, series = series,  verbose = False, n_jobs = -1,  future_covariates=temp_serie)
-    models_list.append(Expon[0])
-    a = Best_model_V2(models_list, series, metric = 'MAPE', exog = temp_serie)
-    a['Output'].plot(label = str(a["Model"]))
-    winsound.Beep(440, 100)
-    plt.ylim(0,10)
-    plt.savefig('model.pdf')
-    plt.show()
-
+  i = 0    
+  for d in d_range:
+    Data.loc[Last_Index + i , 'Date'] = d
+    i+= 1
+      
   
-To_Date_Data(Fuel_Data, Fuel_list)
+  for col in Data.columns[1:]:
+    #Fuel = fuel_dict['in_fuel_price'][Fuel_list[0]]
+    test = Data[col].dropna()
+    #print('Test:', test)
+    Last_Index = test.index[-1]
+    Last_Date = Data.loc[Last_Index,'Date']
+    print('Col: ',col)
+    print('Last Index: ', Last_Index)
+    print('Last Date: ', Last_Date)
+    print('Forecast Date: ', Forecast_Date)
+    
+    Last_Value = Data.loc[Last_Index, col]
+    FORECAST_PERIOD = max((Forecast_Date - Last_Date).days,0)
+    d_range = pd.date_range(start = Last_Date, end = Forecast_Date)
+      
+    if col not in var_list:
+      i = 1
+      for d in d_range:
+        Data.loc[Last_Index + i , col] = Last_Value 
+        i+= 1
+        #Data.loc[(Data['Date'] <= Forecast_Date) & (Data['Date'] >= Last_Date), Fuel] = Last_Value
+
+    else:  
+      #Fuel = fuel_dict['in_fuel_price'][col]
+      df = Data[['Date', col]]
+      df['Date'] = pd.DatetimeIndex(df['Date'])
+      df = df.loc[(df['Date'] <= Last_Date) & (df['Date'] >= Start_Date)]
+      temp['Date'] = pd.DatetimeIndex(temp['Date'])    
+      series = TimeSeries.from_dataframe(df, time_col = 'Date', freq = 'd', fill_missing_dates = True)
+      series.plot(label = col)
+      temp_serie = TimeSeries.from_dataframe(temp, time_col = 'Date', freq = 'd', fill_missing_dates = True)
+      
+      models_list = []
+      #Tet = FourTheta.gridsearch(parameters = parameters["T"], series = series, forecast_horizon = FORECAST_PERIOD, metric = metrics.metrics.mape, n_jobs = -1 ,  future_covariates=temp_serie)
+      #models_list.append(Tet[0])
+      #Arima_model = ARIMA.gridsearch(parameters = parameters["ARIMA"], forecast_horizon = FORECAST_PERIOD, series = series,  verbose = False, n_jobs = -1,  future_covariates=temp_serie)
+      #models_list.append(Arima_model[0])
+      Expon = ExponentialSmoothing.gridsearch(parameters = parameters["ExponentialSmoothing"], forecast_horizon= FORECAST_PERIOD, series = series,  verbose = False, n_jobs = -1,  future_covariates=temp_serie)
+      models_list.append(Expon[0])
+      a = Best_model_V2(models_list, series, metric = 'MAPE', exog = temp_serie)
+      f = a['Output'].pd_dataframe()
+      f.to_csv('time_series.csv')
+      print('f.iloc[0, 0]',f.iloc[0, 0])
+      i = 1
+      for d in d_range:
+        Data.loc[Last_Index + i , col] = f.iloc[i+1, 0]
+        i+= 1
+
+      a['Output'].plot(label = str(a["Model"]))
+      winsound.Beep(440, 100)
+      plt.ylim(0,16)
+      #plt.savefig('model.pdf')
+      plt.show()
+  
+  #print(Data.tail)
+  Data.to_csv('Output_file.csv')
+
+To_Date_Data(Fuel_Data, Fuel_list, FORECAST_PERIOD)
+
 
 '''
+Last_Index = Fuel_Data.index[-1]
+Last_Date = Fuel_Data.iloc[Last_Index, 0]
+Today = datetime.date.today()
+Forecast_Date = Today + timedelta(days = FORECAST_PERIOD)
+Forecast_Date =  pd.to_datetime(Forecast_Date)
+print('Last Index: ', Last_Index)
+print('Last Date: ', Last_Date)
+print('Forecast Date: ', Forecast_Date)
+  
+d_range = pd.date_range(start = Last_Date, end = Forecast_Date , freq='D')
+print('d_range', d_range)
+    
+i = 1
+for d in d_range:
+  Fuel_Data.loc[Last_Index + i , 'Date'] = d
+  i+= 1
+print(Fuel_Data.tail())
+
+Fuel_Data.to_csv('Output_file.csv')
+    
+    
+
+
+#print('Fuel List:', Fuel_list)
+#To_Date_Data(Fuel_Data, Fuel_list, FORECAST_PERIOD)
+
+
 df = Fuel_Data[['Date', Fuel]]
 Start_Date = pd.datetime(2021,5,1)
 
